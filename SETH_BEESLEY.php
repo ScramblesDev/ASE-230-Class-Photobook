f43524242f0f7cd3a268d536cd88c5d2187a7f21
ScramblesDev <beesley02@gmail.com>
2022-09-12 15:36:45 -0400

    Add files via upload

---
+++ SETH_BEESLEY.php
@@ -0,0 +1,94 @@
+<html lang="en">
+	<!-- https://www.bootdey.com/snippets/view/team-user-resume#html -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" integrity="sha256-mmgLkCYLUQbXn0B1SRqzHar6dCnv9oZFPEC1g1cwlkk=" crossorigin="anonymous" />
+
+		
+	<body>
+		<link rel="stylesheet" href="assets/css/detail.css" />
+		<title><?php echo "ASE 230 - Seth Beesley"; ?></title>
+		<div class="container text-center mb-5">
+			<h1><?php echo "This is ASE 230 - Seth Beesley"; ?></h1>
+		</div>
+		<div class="container">
+    <div class="row">
+      <div class="col-lg-5 col-md-6">
+        <div class="mb-2">
+          <img class="w-100" src="https://media-exp1.licdn.com/dms/image/C5603AQHZUFNBdtTHnQ/profile-displayphoto-shrink_200_200/0/1657853623221?e=1667433600&v=beta&t=wPbXk1nDxBycVUh2Gcmfhw_YioMPFLr4x5egJ5u6P2o" alt="">
+        </div>
+        <div class="mb-2 d-flex">
+          <h4 class="font-weight-normal"><?php echo "Seth Beesley"; ?></h4>
+          <div class="social d-flex ml-auto">
+            <p class="pr-2 font-weight-normal"><?php echo "Follow on:"; ?></p>
+            <a href="#" class="text-muted mr-1">
+              <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="#" class="text-muted mr-1">
+              <i class="fab fa-twitter"></i>
+            </a>
+            <a href="#" class="text-muted">
+              <i class="fab fa-linkedin"></i>
+            </a>
+          </div>
+        </div>
+        <div class="mb-2">
+          <ul class="list-unstyled">
+            <li class="media">
+              <span class="w-25 text-black font-weight-normal">Dream profession:</span>
+              <label class="media-body"><?php echo "Web Developer"; ?></label>
+            </li>
+            <li class="media">
+              <span class="w-25 text-black font-weight-normal">Dream company: </span>
+              <label class="media-body"><?php echo "Google"; ?></label>
+            </li>
+            <li class="media">
+              <span class="w-25 text-black font-weight-normal">Email: </span>
+              <label class="media-body"><?php echo "beesleys1@nku.edu" ?></label>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-lg-7 col-md-6 pl-xl-3">
+        <h5 class="font-weight-normal">Short intro</h5>
+        <p><?php echo "Hello! my name is Seth Beesley. I'm a junior at Northern Kentucky University, and my major is Applied Software Engineering, and my minor is Electronic Media and Broadcasting. "; ?></p>
+        <div class="my-2 bg-light p-2">
+          <p class="font-italic mb-0"><?php echo "Nothing is original. Steal from anywhere that resonates with inspiration or fuels your imagination. Devour old films, new films, music, books, paintings, photographs, poems, dreams, random conversations, architecture, bridges, street signs, trees, clouds, bodies of water, light and shadows. Select only things to steal from that speak directly to your soul. If you do this, your work (and theft) will be authentic."; ?></p>
+        </div>
+        <div class="mb-2 mt-2 pt-1">
+          <h5 class="font-weight-normal">Top skills</h5>
+        </div>
+        <div class="py-1">
+          <div class="progress">
+            <div class="progress-bar" role="progressbar" style="width:85%" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">
+              <div class="progress-bar-title">Finance</div>
+              <span class="progress-bar-number">85%</span>
+            </div>
+          </div>
+        </div>
+        <div class="py-1">
+          <div class="progress">
+            <div class="progress-bar" role="progressbar" style="width:70%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
+              <div class="progress-bar-title">Information Technologies</div>
+              <span class="progress-bar-number">70%</span>
+            </div>
+          </div>
+        </div>
+        <div class="py-1">
+          <div class="progress">
+            <div class="progress-bar" role="progressbar" style="width:77%" aria-valuenow="77" aria-valuemin="0" aria-valuemax="100">
+              <div class="progress-bar-title">Education</div>
+              <span class="progress-bar-number">77%</span>
+            </div>
+          </div>
+        </div>
+        <h5 class="font-weight-normal">Fun fact</h5>
+        <p><?php echo "I enjoy playing with synthesizers."?></p>
+        
+      <a href="index.php">Back to photobook</a>
+
+      </div>
+    </div>
+  </div>
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
---
+++ index.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- https://www.bootdey.com/snippets/view/single-advisor-profile#html -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+        <link rel="stylesheet" href="assets/css/index.css" />
+        <title><?php echo "ASE 230 - class of Spring/Fall/Summer 20XX"; ?></title>
+    </head>
+    <body>
+        <div class="container text-center">
+            <h1> <?php echo "This is ASE 230 - class of Spring/Fall/Summer 20XX"; ?></h1>
+        </div>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 col-sm-8 col-lg-6">
+                    <!-- Section Heading-->
+                    <div class="section_heading text-center wow fadeInUp" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
+                        <h3>Our Creative <span> Team</span></h3>
+                        <p>Appland is completely creative, lightweight, clean &amp; super responsive app landing page.</p>
+                        <div class="line"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <!-- Single Advisor-->
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
+                        <!-- Team Thumb-->
+                        <div class="advisor_thumb"><a href="SETH_BEESLEY.php"><img src="https://media-exp1.licdn.com/dms/image/C5603AQHZUFNBdtTHnQ/profile-displayphoto-shrink_200_200/0/1657853623221?e=1667433600&v=beta&t=wPbXk1nDxBycVUh2Gcmfhw_YioMPFLr4x5egJ5u6P2o" alt=""></a>
+                        <!-- Social Info-->
+                        <div class="social-info"><a href="https://www.facebook.com/seth.beesley.7"><i class="fa fa-facebook"></i></a><a href="https://twitter.com/scramblesart"><i class="fa fa-twitter"></i></a><a href="https://www.linkedin.com/in/seth-beesley-2369b41b2/"><i class="fa fa-linkedin"></i></a></div>
+                        </div>
+                        <!-- Team Details-->
+                        <div class="single_advisor_details_info">
+                        <h6>
+                            <?php
+                                echo "Seth Beesley";
+                            ?>
+                        </h6>
+                        <p class="designation">Software Engineering Student</p>
+                    </div>
+                </div>
+            </div>
+            <!-- Single Advisor-->
+            <div class="col-12 col-sm-6 col-lg-3">
+                <div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.3s" style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInUp;">
+                    <!-- Team Thumb-->
+                    <div class="advisor_thumb"><a href="SETH_BEESLEY.php"><img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt=""></a>
+                        <!-- Social Info-->
+                        <div class="social-info"><a href="detail.php"><i class="fa fa-facebook"></i></a><a href="detail.php"><i class="fa fa-twitter"></i></a><a href="detail.php"><i class="fa fa-linkedin"></i></a></div>
+                    </div>
+                    <!-- Team Details-->
+                    <div class="single_advisor_details_info">
+                        <h6>Nazrul Islam</h6>
+                        <p class="designation">UI Designer</p>
+                    </div>
+                </div>
+            </div>
+            <!-- Single Advisor-->
+            <div class="col-12 col-sm-6 col-lg-3">
+                <div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">
+                    <!-- Team Thumb-->
+                    <div class="advisor_thumb"><a href="detail.php"><img src="https://bootdey.com/img/Content/avatar/avatar6.png" alt=""></a>
+                        <!-- Social Info-->
+                        <div class="social-info"><a href="detail.php"><i class="fa fa-facebook"></i></a><a href="detail.php"><i class="fa fa-twitter"></i></a><a href="detail.php"><i class="fa fa-linkedin"></i></a></div>
+                    </div>
+                    <!-- Team Details-->
+                    <div class="single_advisor_details_info">
+                    <h6>Riyadh Khan</h6>
+                    <p class="designation">Developer</p>
+                </div>
+            </div>
+        </div>
+        <!-- Single Advisor-->
+            <div class="col-12 col-sm-6 col-lg-3">
+                <div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInUp;">
+                    <!-- Team Thumb-->
+                    <div class="advisor_thumb"><a href="detail.php"><img src="https://bootdey.com/img/Content/avatar/avatar2.png" alt=""></a>
+                        <!-- Social Info-->
+                        <div class="social-info"><a href="detail.php"><i class="fa fa-facebook"></i></a><a href="detail.php"><i class="fa fa-twitter"></i></a><a href="detail.php"><i class="fa fa-linkedin"></i></a></div>
+                    </div>
+                    <!-- Team Details-->
+                    <div class="single_advisor_details_info">
+                        <h6>Niloy Islam</h6>
+                        <p class="designation">Marketing Manager</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+</html>
